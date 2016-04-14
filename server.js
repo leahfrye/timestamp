c91c5990c3b1f7893fe562f12fb2414748ef759d
leah <lefrye@nps.edu>
2016-04-14 12:28:33 -0700

    update server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ var express = require('express'),
 
 var app = express();
 
-mongo.connect(process.env.'mongodb://heroku_rfjc658z:svqf4ri8vsknfqhodrosgj0rrd@ds023500.mlab.com:23500/heroku_rfjc658z'
- || 'mongodb://localhost:27017/clementinejs', function (err, db) {
+mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
 
     if (err) {
         throw new Error('Database failed to connect!');
@@ -20,7 +19,7 @@ mongo.connect(process.env.'mongodb://heroku_rfjc658z:svqf4ri8vsknfqhodrosgj0rrd@
 
     routes(app, db);
 
-    app.listen(process.env.PORT, function () {
+    app.listen(process.env.PORT || <default port>, function () {
         console.log('Listening on port 3000...');
     });
 
